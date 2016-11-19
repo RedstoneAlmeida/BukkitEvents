8c41fad74a84b4f8a5062560610080492e5849f3
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-11-19 14:31:25 -0200

    Update Loader.php

--- src/BE/Loader.php
+++ src/BE/Loader.php
@@ -5,11 +5,58 @@ namespace BE;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 
+use pocketmine\event\inventory\InventoryTransactionEvent;
+use BE\event\InventoryClickEvent;
+
 class Loader extends PluginBase implements Listener{
+
+    public static $instance;
+
+    public static function getInstance(){
+        return self::$instance;
+    }
 
     public function onEnable()
     {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onTransaction(InventoryTransactionEvent $ev){
+        $failed = [];
+        foreach(Server::getInstance()->getOnlinePlayers() as $player) {
+            while (!$ev->getQueue()->getTransactions()->isEmpty()) {
+                if (!$this->getInv($player)) {
+                    return;
+                }
+                $transaction = $ev->getQueue()->getTransactions()->dequeue();
+                if ($transaction->getInventory() instanceof ContainerInventory || $transaction->getInventory() instanceof PlayerInventory) {
+                    $player->getServer()->getPluginManager()->callEvent($event = new InventoryClickEvent($transaction->getInventory(), $player, $transaction->getSlot(), $transaction->getInventory()->getItem($transaction->getSlot())));
+                    if ($event->isCancelled()) {
+                        $ev->setCancelled(true);
+                    }
+                    if ($ev->isCancelled()) {
+                        $transaction->sendSlotUpdate($player);
+                        continue;
+                    } elseif (!$transaction->execute($player)) {
+                        $transaction->addFailure();
+                        if ($transaction->getFailures() >= SimpleTransactionQueue::DEFAULT_ALLOWED_RETRIES) {
+                            $failed[] = $transaction;
+                        } else {
+                            $transaction->sendSlotUpdate($player);
+                            $ev->getQueue()->getTransactions()->enqueue($transaction);
+                        }
+                        continue;
+                    }
+                   
+                    $transaction->setSuccess();
+                    $transaction->sendSlotUpdate($player);
+
+                    foreach ($failed as $f) {
+                        $f->sendSlotUpdate($player);
+                    }
+                }
+            }
+        }
     }
 
 }
