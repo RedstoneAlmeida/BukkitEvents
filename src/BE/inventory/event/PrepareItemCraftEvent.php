f0a5c899f0d5a4e4ff4a0ea828cf73c454407f78
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-11-19 15:11:15 -0200

    Update PrepareItemCraftEvent.php

--- src/BE/inventory/event/PrepareItemCraftEvent.php
+++ src/BE/inventory/event/PrepareItemCraftEvent.php
@@ -7,7 +7,33 @@ use pocketmine\inventory\Inventory;
 use pocketmine\item\Item;
 use pocketmine\Player;
 use pocketmine\event\inventory\InventoryEvent;
+use pocketmine\inventory\CraftingInventory;
 
 class PrepareItemCraftEvent extends InventoryEvent implements Cancellable{
+  
+  public static $handlerList = null;
+  private $repair;
+  private $what;
+  
+  public function __construct(CraftingInventory $what, InventoryEvent $view, (bool) $repair){
+    $this->what = $what;
+    $this->view = $view->getViewers();
+    $this->repair = $repair;
+  }
 
+  public function getRecipe(){
+    return $this->what->getRecipe();
+  }
+
+  public function getInventory(){
+    return $this->what;
+  }
+  
+  /**
+     * @return Player
+   */
+  public function getWho(){
+    return $this->view;
+  }
+  
 }
